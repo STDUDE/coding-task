c68240246028ad1a1443e5c28a4218444e1a8ca2
STDude <antonina.klimovich@gmail.com>
2020-09-03 00:03:44 +0300

    Remove unnecessary import

--- src/main/java/com/test/codingtask/config/security/AuthServerConfig.java
+++ src/main/java/com/test/codingtask/config/security/AuthServerConfig.java
@@ -1,7 +1,6 @@
 package com.test.codingtask.config.security;
 
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.crypto.password.PasswordEncoder;
